7634d80df15c16b466f07c87bbee8b0b28ff2375
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-08-21 08:56:33 +0200

    Allow no default environment variable

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -4,5 +4,5 @@ data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt()
 )
 
-fun getEnvVar(varName: String, defaultValue: String) =
+fun getEnvVar(varName: String, defaultValue: String? = null) =
         System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
