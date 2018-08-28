b807e746e89d12846785bbe19ab6a6184fbb49b4
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-08-28 13:38:41 +0200

    Rework application state to work better with multiple workers

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -4,15 +4,13 @@ import io.ktor.application.Application
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import no.nav.syfo.api.registerNaisApi
 import java.util.concurrent.TimeUnit
 
-fun doReadynessCheck(): Boolean {
-    // Do validation
-    return true
-}
-
-data class ApplicationState(var running: Boolean = true)
+data class ApplicationState(var running: Boolean = true, var initialized: Boolean = false)
 
 
 fun main(args: Array<String>) {
@@ -23,13 +21,41 @@ fun main(args: Array<String>) {
         initRouting(applicationState)
     }.start(wait = true)
 
-    Runtime.getRuntime().addShutdownHook(Thread {
-        applicationServer.stop(10, 10, TimeUnit.SECONDS)
-    })
+    try {
+        val listeners = (1..env.applicationThreads).map {
+            launch {
+                blockingApplicationLogic(applicationState)
+            }
+        }.toList()
+
+        runBlocking {
+            Runtime.getRuntime().addShutdownHook(Thread {
+                applicationServer.stop(10, 10, TimeUnit.SECONDS)
+            })
+
+            applicationState.initialized = true
+            listeners.forEach { it.join() }
+        }
+    } finally {
+        applicationState.running = false
+    }
+}
+
+suspend fun blockingApplicationLogic(applicationState: ApplicationState) {
+    while (!applicationState.running) {
+        delay(100)
+    }
 }
 
 fun Application.initRouting(applicationState: ApplicationState) {
     routing {
-        registerNaisApi(readynessCheck = ::doReadynessCheck, livenessCheck = { applicationState.running })
+        registerNaisApi(
+                readynessCheck = {
+                    applicationState.initialized
+                },
+                livenessCheck = {
+                    applicationState.running
+                }
+        )
     }
 }
