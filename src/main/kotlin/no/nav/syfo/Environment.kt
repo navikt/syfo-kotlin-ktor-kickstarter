389e5abc722c349d74c18d677598356c3a024ed6
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-09-10 09:41:47 +0200

    Add example for using IBM MQ

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -4,7 +4,15 @@ data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
     val srvappnameUsername: String = getEnvVar("SRVAPPNAME_USERNAME"),
-    val srvappnamePassword: String = getEnvVar("SRVAPPNAME_PASSWORD")
+    val srvappnamePassword: String = getEnvVar("SRVAPPNAME_PASSWORD"),
+    val mqQueueManagerName: String = getEnvVar("MQGATEWAY04_NAME"),
+    val mqHostname: String = getEnvVar("MQGATEWAY04_HOSTNAME"),
+    val mqPort: Int = getEnvVar("MQGATEWAY04_PORT").toInt(),
+    val mqGatewayName: String = getEnvVar("MQGATEWAY04_NAME"),
+    val mqChannelName: String = getEnvVar("#APP_NAME#"),
+    val srvappserverUsername: String = getEnvVar("SRVAPPSERVER_USERNAME", "srvappserver"),
+    val srvappserverPassword: String = getEnvVar("SRVAPPSERVER_PASSWORD", ""),
+    val sampleQueueQueuename: String = getEnvVar("SAMPLE_QUEUE_QUEUENAME")
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
